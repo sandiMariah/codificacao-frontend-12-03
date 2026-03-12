a72fa4277d9c54b7ed2b9d782557151b45859005
mariah sandim <sandim.mariah@gmail.com>
2026-03-12 16:01:46 -0300

    Create atividade.js

---
+++ 12-03-26/atividade.js
@@ -0,0 +1,45 @@
+// let num1 = 2
+// let num2 = 6
+
+// console.log(num1+num2);
+
+////////// 
+
+
+// let num1 = 1
+// let num2 = 4
+
+// console.log(num1-num2);
+
+///////////
+
+// let num1 = 3
+// let num2 = 7
+
+
+// console.log(num1*num2);
+
+//////////////////////
+
+
+// let num1 = 10
+// let num2 = 5
+
+
+// console.log(num1/num2);
+
+///////////////////
+
+// let num1 = 22
+// let num2 = 7
+
+// console.log(num1 % num2);
+
+////////////////////
+
+// let num1 = 9
+// let num2 = 3
+
+// console.log(num1**num2);
+
+
