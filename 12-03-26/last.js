ac1ea2615882ae3a954f06ed51d1cc2e3f50d85a
mariah sandim <sandim.mariah@gmail.com>
2026-03-12 16:57:58 -0300

    Atividade de validação de idade, preço e aplicação de desconto

---
+++ 12-03-26/last.js
@@ -0,0 +1,14 @@
+let idade = 60;
+let preco = 100;
+
+if(idade > 60 && preco > 100){
+
+    let precoFinal = preco - (preco * 0.20)
+
+    console.log('Desconto Aplicado ! Novo preço: ' + precoFinal);
+}
+
+    else{
+        console.log('Não foi possivel aplicar o desconto');
+        
+    }
