467f4dd4dff4edee5240f1375a4cecc612fae632
mariah sandim <sandim.mariah@gmail.com>
2026-03-12 16:26:18 -0300

    atividade de verdadeiro ou falso 2

--- 12-03-26/atividade.js
+++ 12-03-26/atividade.js
@@ -45,3 +45,74 @@
 
 // ATIVIDADE 
 
+// # Exercícios: Operadores Relacionais
+// ## O objetivo é determinar se a expressão resulta em Verdadeiro (True) ou Falso (False).
+
+// ### 1. Operador: Igual a (==)
+// ``` 
+// Verifique se o resultado da operação matemática abaixo é igual ao valor comparado:
+
+// Expressão: (15 / 3) == 5
+
+// Pergunta: O resultado é Verdadeiro ou Falso?
+// Resposta: VERDADEIRO
+// Explicação da resposta: Dois sinais de igual significa igualdade 
+// ```
+// ### 2. Operador: Diferente de (!=)
+// ```
+// Analise a comparação entre as duas strings (textos) abaixo:
+
+// Expressão: "Python" != "python"
+
+// Pergunta: Considerando que a maioria das linguagens diferencia maiúsculas de minúsculas, qual o resultado?
+// Resposta: VERDADEIRO
+// Explicação da resposta: São duas strings diferentes
+// ```
+// ### 3. Operador: Maior que (>)
+// ```
+// Imagine que um sistema de e-commerce verifica o estoque de um produto:
+
+// Cenário: estoque = 10, pedido = 12.
+
+// Expressão: pedido > estoque
+
+// Pergunta: O sistema deve bloquear a venda? (O resultado é Verdadeiro ou Falso?)
+// Resposta: Verdadeiro
+// Explicação da resposta: O numero de pedidos e maior que a do estoque então o sistema deve bloquear a venda
+// ```
+// ### 4. Operador: Menor que (<)
+// ```
+// Verifique a idade mínima para uma categoria infantil de esportes:
+
+// Cenário: idade_atleta = 9.
+
+// Expressão: idade_atleta < 10
+
+// Pergunta: O atleta pode participar da categoria Sub-10?
+// Resposta: Sim, pois o dez não esta incluso na expressão somente o antecessor
+// Explicação da resposta:
+// ```
+// ### 5. Operador: Maior ou igual a (>=)
+// ```
+// Um sistema de cinema concede meia-entrada para idosos (60 anos ou mais):
+
+// Cenário: cliente_idade = 60.
+
+// Expressão: cliente_idade >= 60
+
+// Pergunta: O cliente tem direito ao benefício?
+// Resposta: Sim, poi o 60 está incluido na expressão
+// Explicação da resposta:
+// ```
+// ### 6. Operador: Menor ou igual a (<=)
+// ```
+// Um sensor de temperatura dispara um alerta se o clima estiver abaixo de zero ou congelando:
+
+// Cenário: temperatura_atual = 0.
+
+// Expressão: temperatura_atual <= 0
+
+// Pergunta: O alerta será disparado?
+// Resposta: Falso
+// Explicação da resposta: Não, pois o zero está incluido na expressão
+// ```
